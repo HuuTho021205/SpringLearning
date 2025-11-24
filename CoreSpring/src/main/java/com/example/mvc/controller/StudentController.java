09d3d84e50e69f0f0f4d5408fe243383926b4432
HuuTho021205 <trandinhhuutho@gmail.com>
2025-11-24 20:41:55 +0700

    update thêm ResponseEntity

--- CoreSpring/src/main/java/com/example/mvc/controller/StudentController.java
+++ CoreSpring/src/main/java/com/example/mvc/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.mvc.controller;
 import com.example.mvc.model.Student;
 import com.example.mvc.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,27 +15,31 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
-    public List<Student> getList(){
+    public ResponseEntity<List<Student>> getList(){
         System.out.println("Có khách đang xem danh sách sinh viên");
-        return studentService.getAll();
+        return ResponseEntity.ok(studentService.getAll());
     }
 
     @PostMapping
-    public Student add(@RequestBody Student student){
+    public ResponseEntity<Student> add(@RequestBody Student student){
         System.out.println("Có khách đang thêm sinh viên mới: " + student.getName());
-        return studentService.add(student);
+        return ResponseEntity.ok(studentService.add(student));
     }
 
     @PutMapping("/{id}")
-    public Student update(@PathVariable Long id, @RequestBody Student student){
+    public ResponseEntity<Student> update(@PathVariable Long id, @RequestBody Student student){
         System.out.println("Khách muốn sửa sinh viên: " + id);
-        return studentService.update(id, student);
+        Student result = studentService.update(id, student);
+        if (result == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable Long id){
+    public ResponseEntity<String> delete(@PathVariable Long id){
         System.out.println("Khách muốn xóa sinh viên: " + id);
         studentService.detete(id);
-        return "Đã xóa thành công sinh viên: " + id;
+        return ResponseEntity.ok("Đã xóa thành công sinh viên: " + id);
     }
 }
